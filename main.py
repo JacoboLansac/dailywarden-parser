934095ab64ac7d23f86da4218c5a630652ffbd65
jl <jacolansac@gmail.com>
2023-11-13 16:44:51 +0100

    requirements and tabulate

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     parser.add_argument('-nr', '--number-of-rows', default=10, type=int)
     args = parser.parse_args()
 
-    with open("email_to_parse.txt", 'r') as f:
+    with open("email", 'r') as f:
         textdata = f.read()
 
     lines = textdata.split("\n")
@@ -17,13 +17,16 @@ if __name__ == '__main__':
     df = pd.DataFrame(np.array(info).reshape(-1, 6), columns=['Platform', 'Sponsor', 'Pot', 'Sloc', 'Starts', 'Ends'])
 
     df['pot_preprocessed'] = df['Pot'].str.extract('.*\$(\d+,\d+).*')
-    df['pot_usd'] = df['pot_preprocessed'].str.replace(',','').astype(int)
+    df['pot_usd'] = df['pot_preprocessed'].str.replace(',','').astype(float)
 
     df['sloc'] = df['Sloc'].str.replace(',','').replace('N/A', np.nan).astype(float)
     df['usdc_per_solc'] = df['pot_usd'] / df['sloc']
 
     df.sort_values(by='usdc_per_solc', ascending=False, inplace=True)
-
+    df.drop_duplicates(subset=['Platform', 'Sponsor'], inplace=True)
 
     print(f"Best payouts per SLOC:")
-    print(df[['Platform', 'Sponsor', 'sloc', 'usdc_per_solc', 'pot_usd']].head(args.number_of_rows))
+    for i, row in df.head(args.number_of_rows).iterrows():
+        print(row)
+
+    # print(df[['Platform', 'Sponsor', 'sloc', 'usdc_per_solc', 'pot_usd']].head(args.number_of_rows))
