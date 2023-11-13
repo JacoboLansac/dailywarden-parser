31214516163a809888998a55a86064934647538c
jl <jacolansac@gmail.com>
2023-11-13 12:02:49 +0100

    repo ready for parsing

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # dailywarden-parser
 A small script to parse the emails from daily warden and get some stats from the contests
+
+## How to use
+
+Just copy paste the email into `email_to_parse.txt` and run 
+
+    python3 main.py
---
+++ main.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+import argparse
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-nr', '--number-of-rows', default=10, type=int)
+    args = parser.parse_args()
+
+    with open("email_to_parse.txt", 'r') as f:
+        textdata = f.read()
+
+    lines = textdata.split("\n")
+    info = [line for line in lines if ('\t' not in line) and (line != '') and ('Contest' not in line)]
+
+    df = pd.DataFrame(np.array(info).reshape(-1, 6), columns=['Platform', 'Sponsor', 'Pot', 'Sloc', 'Starts', 'Ends'])
+
+    df['pot_preprocessed'] = df['Pot'].str.extract('.*\$(\d+,\d+).*')
+    df['pot_usd'] = df['pot_preprocessed'].str.replace(',','').astype(int)
+
+    df['sloc'] = df['Sloc'].str.replace(',','').replace('N/A', np.nan).astype(float)
+    df['usdc_per_solc'] = df['pot_usd'] / df['sloc']
+
+    df.sort_values(by='usdc_per_solc', ascending=False, inplace=True)
+
+
+    print(f"Best payouts per SLOC:")
+    print(df[['Platform', 'Sponsor', 'sloc', 'usdc_per_solc', 'pot_usd']].head(args.number_of_rows))
